f63af9c095add4f8349b862d643505d3afaf8deb
Charlene <charlene@peters-services.com>
2022-02-21 17:09:28 -0600

    creating simpler version of storylinecreator

---
+++ js/script.js
@@ -0,0 +1,139 @@
+var newAnalysis = document.getElementById('newAnalysis');
+var newConnection = document.getElementById('newConnection');
+var nextVideo = document.getElementById('nextVideo');
+//var vidPlayer = document.getElementById("videoPlayer");
+var connectionIDPlace = document.getElementById("connectionID");
+var currentTimePlace = document.getElementById("currentTimeHereNow");
+var uploadButton = document.getElementById("uploadFile");
+var getEntries = document.getElementById("getEntries");
+var jsonRead = document.getElementById("jsonRead");
+var videoURL = document.getElementById("videoURL");
+//var submit = document.getElementById("submit");
+var start = document.getElementById('newVideo');
+var myVar, counter; 
+var username = "CharleneP";
+var actionJSON = [];
+buttons();
+
+//start.addEventListener("click", function (e) {
+
+//})
+
+
+start.addEventListener("click", function (e) {
+  e.preventDefault();
+  //var videoTest = JSON.parse(this.responseText);
+  console.log(videoURL.value);
+      var video = videoURL.value;
+      var vidPlayer = document.getElementById("videoPlayer");
+      //var vidURL = document.getElementById("videoURL");
+      //vidURL.innerText = video;
+//**Video Player */
+
+      //Youtube Videos  
+        var output = '';
+      output += `<iframe id="videoChange" width="560" height="315" src="` + video + `" title="YouTube 
+      video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
+      gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+       //Output for videos not on Youtube and Vimeo
+      /*
+            output += `<video id="videoChange" width="500" height="400" controls>
+            <source src="` + videoTest[0].url + `" type="video/` + videoTest[0].media + `" />
+          Your browser does not support the video tag.
+          </video>`;
+      */
+
+      vidPlayer.innerHTML = output;
+
+      return video;
+})
+
+function buttons() {
+  var actions = ["Aerobic/Cardio Exercise", "Affectionate", "Agreement", "Analyze", "Angry Movements", "Annoy/Insult", "Arrive", "Balance", "Bend", "Bleed", "Bite", "Carry", "Challenge", "Clean", "Climb", "Close", "Confusion", "Cook", "Cool/Freeze", "Cover", "Creative Arts", "Crime", "Cut", "Decrease", "Desisive Movements", "Die", "Dirty", "Discipline", "Disgust", "Drop", "Eat/Drink", "Fall", "Fearful Movements", "Feeling Positive", "Financial Transaction", "Finger Movement", "Float/Water Movement", "Follow", "Forced Entry", "Gather", "Give", "Grab", "Grow", "Hand Dancing", "Hand Movement - Shake", "Heat/Light Up/Ignite", "Ignore", "Imitate", "Increase", "Inform", "Insert", "Jailed", "Kick", "Kidnap", "Kill", "Leadership Movements", "Learn", "Lift", "Limp", "Listen", "Maintenance", "Material Damage", "Mechanical Operation", "Mouth Movements", "Neutral Hands Connecting With Something", "No Movement", "Non-Life Threatening Bodily Harm", "Non-Verbal Sound From Mouth", "Open", "Organize (Objects)", "Pain (Feel)", "Persuade", "Poke/Tap", "Preserve/Protect", "Pull", "Push", "Quicken", "Read", "Realization", "Redo", "Reject", "Relax", "Relief (Feel)", "Relief (Provide)", "Remove/Send Away/Leave", "Resist", "Reverse", "Rise", "Rub", "Run", "Sadness", "Scrape", "Search", "Separate", "Sexual Attraction", "Shiver", "Shoot", "Show Something", "Sit", "Slow", "Smell", "Social Movements (Inclusive)", "Stimulate", "Stumble", "Succeed", "Sway", "Take", "Throw", "Threaten", "To March/Skip", "To Scare", "Touch", "Trust", "Twist/Turn", "Uncover", "Verbal Speech", "Walk", "Watch", "Wish to Create Positive Emotions", "Write/Document"];
+
+  for (i = 0; i < actions.length; i++) {
+
+    var CreateButton = document.createElement("button");
+    CreateButton.innerText = actions[i];
+    document.getElementById("actions").appendChild(CreateButton);
+    CreateButton.addEventListener("click", function (e) {
+      e.preventDefault();
+
+      var actionAdd = {
+        action: this.innerHTML,
+        date: Date(),
+        currentTime: currentTimePlace.innerHTML,
+        videoID: videoURL.value,
+        connectionID: connectionIDPlace.innerText,
+        username: username
+      }
+      actionJSON.push(actionAdd);
+      console.log(actionJSON);
+    })
+  }
+  return actionJSON;
+}
+
+newAnalysis.addEventListener("click", function (e) {
+  e.preventDefault();
+  var actionAdd = {
+    action: "New Analysis",
+    date: Date(),
+    currentTime: '0',
+    videoID: videoURL.value,
+    connectionID: connectionIDPlace.innerText,
+    username: username
+  }
+  clearInterval(myVar);
+  counter = 0;
+  myVar = setInterval(restartTimer, 1000);
+  
+  actionJSON.push(actionAdd);
+})
+
+
+nextVideo.addEventListener("click", function (e) {
+  e.preventDefault();
+  var actionAdd = {
+    action: "New Video",
+    date: Date(),
+    currentTime: '0',
+    videoID: videoURL.value,
+    connectionID: connectionIDPlace.innerText,
+    username: username
+  }
+  clearInterval(myVar);
+  counter = 0;
+  myVar = setInterval(restartTimer, 1000);
+
+  actionJSON.push(actionAdd);
+})
+
+newConnection.addEventListener("click", function (e) {
+  e.preventDefault();
+  var connectionIDCreation = Math.floor(Math.random() * 100000000);
+  connectionIDPlace.innerHTML = connectionIDCreation;  
+  clearInterval(myVar);
+  counter = 0;
+  myVar = setInterval(restartTimer, 1000);
+})
+
+
+function restartTimer() {
+  counter++;
+  var t = counter;
+ currentTimePlace.innerHTML = t;
+}
+
+uploadButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  localStorage.setItem("actionJSON", JSON.stringify(actionJSON));
+});
+
+getEntries.addEventListener("click", function (e) {
+  e.preventDefault();  
+  jsonRead.innerHTML = localStorage.getItem("actionJSON");
+});
+
+//module.exports = buttons;
---
+++ lighthouse/lighthouse.js
@@ -0,0 +1,21 @@
+
+
+//Make sure Lighthouse NPM is started and run 'lighthouse/lighthouse.js' for this to start.
+(async () => {
+    const fs = require('fs');
+    const lighthouse = require('lighthouse');
+    const chromeLauncher = require('chrome-launcher');
+    const chrome = await chromeLauncher.launch({ chromeFlags: ['--headless'] });
+    const options = { logLevel: 'info', output: 'html', port: chrome.port };
+    const runnerResult = await lighthouse('https://petersvirtualservices.github.io/storylinecreator/public', options);
+
+    // `.report` is the HTML report as a string
+    const reportHtml = runnerResult.report;
+    fs.writeFileSync('lhreport.html', reportHtml);
+
+    // `.lhr` is the Lighthouse Result as a JS object
+    console.log('Report is done for', runnerResult.lhr.finalUrl);
+    console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+
+    await chrome.kill();
+})();
